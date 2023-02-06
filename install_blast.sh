7f5009ae6347ba9a1f3801deebb65083b9811fdd
Pedro Morell <pedro.morell@ebc.uu.se>
2023-02-06 10:02:09 +0100

    Added a script to install BLAST+ locally.

---
+++ install_blast.sh
@@ -0,0 +1,7 @@
+### Installing BLAST+ locally
+#Downloading
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-*+-x64-linux.tar.gz
+#Unzip
+tar -zxvf ncbi-blast-2.13.0+-x64-linux.tar.gz
+#Set Paths
+export PATH=$HOME/tools/BLAST/ncbi-blast-2.13.0+/bin:$PATH
