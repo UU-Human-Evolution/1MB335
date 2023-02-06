8711c4567129fbfe7c8bd6e27f5638250419f0d0
Pedro Morell <pedro.morell@ebc.uu.se>
2023-02-06 10:56:56 +0100

    Update install_blast.sh

--- install_blast.sh
+++ install_blast.sh
@@ -1,7 +1,13 @@
 ### Installing BLAST+ locally
+#move to root
+cd
 #Downloading
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-*+-x64-linux.tar.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-$1+-x64-linux.tar.gz
 #Unzip
-tar -zxvf ncbi-blast-2.13.0+-x64-linux.tar.gz
+tar -zxvf ncbi-blast-$1+-x64-linux.tar.gz
 #Set Paths
-export PATH=$HOME/tools/BLAST/ncbi-blast-2.13.0+/bin:$PATH
+export PATH=$HOME/tools/BLAST/ncbi-blast-$1+/bin:$PATH
+echo export PATH=$HOME/tools/BLAST/ncbi-blast-$1+/bin:$PATH >> .bash_profile
+echo export PATH=$HOME/tools/BLAST/ncbi-blast-$1+/bin:$PATH >> .bashrc
+
+
